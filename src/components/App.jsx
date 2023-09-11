4c8886a1bb5f2cb3da2fa02ad074ee31e214b631
OlgaSld <125216168+OlgaSld@users.noreply.github.com>
2023-09-11 23:18:26 +0300

    add nanoid

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { ContactForm } from './ContactsForm/ContactForm';
 import { ContactsList } from './ContactList/ContactList';
-// import { nanoid } from 'nanoid';
+import { nanoid } from 'nanoid';
 import { Layout, Subtitle, Title } from './Layout';
 import { Filter } from './Filter/Filter';
 import { useLocalStorage } from './LocalStorage';
@@ -13,55 +13,55 @@ const contactData = [
   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
 ];
 
-const getIntialFilters = () => {
-  const savedContacts = localStorage.getItem('contacts');
-  if (savedContacts !== null) {
-    return JSON.parse(savedContacts);
-  }
-};
-
 export const App = () => {
-    const [contacts, setContacts] = useLocalStorage(getIntialFilters, contactData);
-    const [filter, setFilter] = useState('');
+  const [contacts, setContacts] = useLocalStorage('contacts', contactData);
+  const [filter, setFilter] = useState('');
 
-    useEffect(() => {
-        localStorage.setItem('contacts', JSON.stringify(contacts));
-    }, [contacts])
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
-    const addContact = newContact => {
-        setContacts(prevContacts => {
-    if (prevContacts.find(contact => contact.name.toLowerCase() === newContact.name.toLowerCase())) {
-        alert(`${newContact.name} is already in contacts!`);
-        return prevContacts;
-   }
-            return [...prevContacts, newContact];
-    }       
-   )}
+  const addContact = newContact => {
+    if (
+      contacts.find(
+        contact => contact.name.toLowerCase() === newContact.name.toLowerCase()
+      )
+    ) {
+      alert(`${newContact.name} is already in contacts!`);
+      return;
+    }
 
- const changeFilter = e => {
-   setFilter(e.target.value);
- };
-    
-const visibleContacts = contacts.filter(contact => {
-      const filterCont = contact.name
-        .toLowerCase()
-        .includes(filter.toLowerCase());
-      return filterCont;
+    const newContactId = { id: nanoid(), ...newContact };
+
+    setContacts(prevContacts => {
+      return [...prevContacts, newContactId];
     });
-  
-
-const deleteContact = contactId => {
-    setContacts(prevContacts => 
-      prevContacts.filter(contact => contact.id !== contactId));
   };
 
- return (
-      <Layout>
-        <Title>Phonebook</Title>
-        <ContactForm onAdd={addContact} />
-        <Subtitle>Contacts</Subtitle>
-        <Filter value={filter} onChangeFilter={changeFilter} />
-        <ContactsList contacts={visibleContacts} onDelete={deleteContact} />
-      </Layout>
-    );    
-}
+  const changeFilter = e => {
+    setFilter(e.target.value);
+  };
+
+  const visibleContacts = contacts.filter(contact => {
+    const filterCont = contact.name
+      .toLowerCase()
+      .includes(filter.toLowerCase());
+    return filterCont;
+  });
+
+  const deleteContact = contactId => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== contactId)
+    );
+  };
+
+  return (
+    <Layout>
+      <Title>Phonebook</Title>
+      <ContactForm onAdd={addContact} />
+      <Subtitle>Contacts</Subtitle>
+      <Filter value={filter} onChangeFilter={changeFilter} />
+      <ContactsList contacts={visibleContacts} onDelete={deleteContact} />
+    </Layout>
+  );
+};
